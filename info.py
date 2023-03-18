f280a91a10bf603a453ad4dcb0443468a994c3ac
MahirModan <107642456+MahirModan@users.noreply.github.com>
2023-03-18 19:47:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,35 +15,34 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '28445434'))
 API_HASH = environ.get('API_HASH', '8cc2507d26a86d506a65e9af474e2a0f')
-BOT_TOKEN = environ.get('BOT_TOKEN', '5750965723:AAGj-QRuV51Xx0PQOvyNkN-IYqLMtF7YA4E')
+BOT_TOKEN = environ.get('BOT_TOKEN', '5855639065:AAGM3HBDJFvxFaFRnMg3C_Hm8vtdjQCvkNI')
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
-
 PICS = (environ.get('PICS', 'https://telegra.ph/file/6a0726f79acd8300e9a04.jpg')).split()
 NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/46443096bc6895c74a716.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/451f038b4e7c2ddd10dc0.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/5e2d4418525832bc9a1b9.jpg")
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1366310017').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1315219809 5212976806 1355116689').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001879875542').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('AUTH_CHANNEL', '-1001260085440')
-auth_grp = environ.get('AUTH_GROUP', '-689351576')
+auth_channel = environ.get('AUTH_CHANNEL', '')
+auth_grp = environ.get('AUTH_GROUP', '')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 support_chat_id = environ.get('SUPPORT_CHAT_ID', '-689351576')
-reqst_channel = environ.get('REQST_CHANNEL_ID', '-1001260085440')
+reqst_channel = environ.get('REQST_CHANNEL_ID', '-1001879875542')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://AnimeStore:AnimeStore@cluster0.8ihiyyk.mongodb.net/AnimeStore?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "AnimeStore")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://AnimeDump:AnimeDump@cluster0.bymbuc4.mongodb.net/AnimeDump?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "AnimeDump")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
@@ -51,7 +50,7 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+SJJ_e_CtCwNlNDc1')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+NxceH2RwhNNjYWQ1')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+uqkETlCBAGBhYmY1')
 MSG_ALRT = environ.get('MSG_ALRT', 'Wʜᴀᴛ Aʀᴇ Yᴏᴜ Lᴏᴏᴋɪɴɢ Aᴛ ?')
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001796592159'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'CinemaRoom')
